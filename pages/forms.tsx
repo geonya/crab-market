3f2097be79585ef1fd81ad403a73f31acdbcecf3
geony <geony8410@gmail.com>
2022-02-28 16:07:19 +0900

    react-hook-form 2

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -1,28 +1,70 @@
-import { useForm } from "react-hook-form";
+import { FieldErrors, useForm } from "react-hook-form";
+
+interface LoginForm {
+	username: string;
+	email: string;
+	password: string;
+	errors: string;
+}
 
 export default function Forms() {
-	const { register, handleSubmit } = useForm();
-	const onValid = () => {
+	const {
+		register,
+		watch,
+		handleSubmit,
+		formState: { errors },
+		setError,
+		setValue,
+	} = useForm<LoginForm>({
+		mode: "onChange",
+	});
+	const onValid = (data: LoginForm) => {
 		console.log("I'm valid baby");
+		setError("username", { message: "username is already existed" });
 	};
+	const onInvalid = (errors: FieldErrors) => {
+		console.log(errors);
+	};
+
 	return (
-		<form onSubmit={handleSubmit(onValid)}>
+		<form onSubmit={handleSubmit(onValid, onInvalid)}>
 			<input
-				{...register("username", { required: true })}
+				{...register("username", {
+					required: "Username is required.",
+					minLength: {
+						message: "The username should be longer than 5 chars.",
+						value: 5,
+					},
+				})}
 				type="text"
 				placeholder="Username"
 			/>
+			{errors.username?.message}
 			<input
-				{...register("email", { required: true })}
+				{...register("email", {
+					required: "Email is required.",
+					validate: {
+						notGmail: (value) =>
+							!value.includes("@gmail.com") ||
+							"Gmail is not allowed",
+					},
+				})}
 				type="email"
 				placeholder="Email"
+				className={`${
+					Boolean(errors.email?.message)
+						? "border-2 border-red-500 focus:border-red-500 focus:ring-2 focus:ring-red-500"
+						: ""
+				}`}
 			/>
+			{errors.email?.message}
 			<input
-				{...register("password", { required: true })}
+				{...register("password", { required: "Password is required." })}
 				type="password"
 				placeholder="Password"
 			/>
 			<input type="submit" value="Create Account" />
+			{errors.errors?.message}
 		</form>
 	);
 }
